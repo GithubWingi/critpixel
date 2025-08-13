5608a0b0af72233b57ab18cd19be94718840b501
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-08-13 17:30:31 +0200

    debbug html

--- tests/Functional/Auth/LoginTest.php
+++ tests/Functional/Auth/LoginTest.php
@@ -11,7 +11,9 @@ final class LoginTest extends FunctionalTestCase
 {
     public function testThatLoginShouldSucceeded(): void
     {
-        $this->get('/auth/login');
+        $crawler = $this->client->request('GET', '/auth/login');
+        file_put_contents('login.html', $crawler->html());
+
 
         $this->client->submitForm('Se connecter', [
             'email' => 'user+1@email.com',
